8858e1cc50205abdd3917d39a26d56fec9ac3e73
Emilie G <emilie@neo-nomade.com>
2021-05-27 15:30:38 +0200

    try fix agan

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ const dataUrl = process.env.DATABASE_URL || defaultDB
 const [, , DB_USER, DB_USER_PWD, DB_HOST, DB_PORT, DB_NAME] = dataUrl.match(/^(postgres):\/\/(.*):(.*)@(.*):(\d+)\/(.*)$/,)
 
 console.log('CHAUSSSSETTTTTTTEEEEEE')
-console.log('PROCESS ENV =>', process.env.TEST);
+console.log('TEST=>', process.env.TEST);
+console.log('PROCESS ENV =>', process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
