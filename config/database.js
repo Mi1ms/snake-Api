75a02759f1aee01e95eac3391bb2f0720c7903af
Emilie G <emilie@neo-nomade.com>
2021-05-27 15:42:05 +0200

    try fix agan

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const defaultDB = 'postgres://postgres:postgres@127.0.0.1:5432/snakeapi'
-const dataUrl = process.env.DATABASE_URL || defaultDB
+const dataUrl = process.env.DATAMANUAL_URL || defaultDB
+
 
 const [, , DB_USER, DB_USER_PWD, DB_HOST, DB_PORT, DB_NAME] = dataUrl.match(/^(postgres):\/\/(.*):(.*)@(.*):(\d+)\/(.*)$/,)
 
 console.log('CHAUSSSSETTTTTTTEEEEEE')
-console.log('TEST=>', process.env.TEST);
+console.log('TEST=>', process.env.DATAMANUAL_URL);
 console.log('PROCESS ENV =>', process.env);
 
 module.exports = ({ env }) => ({
