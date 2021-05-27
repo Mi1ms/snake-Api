5442dc0eca88fc1d156b3d91aa03dcfef9ce82e9
Emilie G <emilie@neo-nomade.com>
2021-05-27 16:00:49 +0200

    try fix agan

--- config/database.js
+++ config/database.js
@@ -20,7 +20,7 @@ module.exports = ({ env }) => ({
         database: DB_NAME,
         username: DB_USER,
         password: DB_USER_PWD,
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: process.env.DATABASE_SSL,
       },
       options: {}
     },
